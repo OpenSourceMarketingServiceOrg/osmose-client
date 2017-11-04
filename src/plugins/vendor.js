99d28d9aa232a7e8c276eb01f71a976e6ca91a61
qtlawrence <qtlawrence@outlook.com>
2017-11-03 20:40:18 -0500

    New style framework

--- src/plugins/vendor.js
+++ src/plugins/vendor.js
@@ -1,6 +1,5 @@
-// import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import 'vuetify/dist/vuetify.min.css';
+import 'materialize-css';
 
 // Our stylesheets
 import '../assets/style/index.scss';
--- src/plugins/vuex.js
+++ src/plugins/vuex.js
@@ -11,7 +11,5 @@
 
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Vuetify from 'vuetify';
 
 Vue.use(Vuex);
-Vue.use(Vuetify);
