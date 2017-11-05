f3a78ee18e46d4cce586e5faafd744969fa9bba9
Danjenator <danjenator@gmail.com>
2017-11-05 01:32:24 -0600

    getting close

--- src/pages/Email.vue
+++ src/pages/Email.vue
@@ -2,19 +2,22 @@
     <div class="row ">
       <div class="col s12">
         <form @submit.prevent="saveContent(email)">
-          <client-grid></client-grid>
-          
+          <client-grid :grid-data="emailList"></client-grid>
+          <div class="col s6">
+            <div class="input-field">
+              <input v-model="email.from" id="email_from" type="text" class="validate">
+              <label for="email_from">From</label>
+            </div>
+          </div>
           <div class="col s12">
             <div class="input-field ">
               <input v-model="email.subject" id="email_subject" type="text" class="validate">
               <label for="email_subject">Subject</label>
             </div>
           </div>
-
           <div class="col s12">
             <vue-editor v-model="email.content"></vue-editor>
           </div>
-
           <div class="col s12 pad-me">
               <button class="btn btn-outline-success">
                   <span class="fa fa-paper-plane" style="margin-right:5px;"></span> Send
@@ -25,38 +28,18 @@
     </div>
 </template>
 
-<<<<<<< HEAD
-<!-- <div class="form-group">
-            <div class="input-group input-group-lg">
-              <div class="input-group-addon">
-                <i class="fa fa-user fa-fw"></i>
-              </div>
-              <input v-model="email.sendTo" type="text" placeholder="Send To" class="form-control" />
-            </div>
-          </div> -->
-=======
->>>>>>> 91d564c9a5383a423d90ecb40f2c326649e3b322
-
 <style lang="scss" scoped>
 
   .pad-me {
     margin: 10px auto 10px auto;
   }
 
-
 </style>
 
 <script>
-<<<<<<< HEAD
 
 import { VueEditor } from 'vue2-editor';
 import ClientGrid from '@/components/ClientGrid';
-=======
-  import { VueEditor } from 'vue2-editor';
-  import ClientGrid from '@/components/ClientGrid';
-
- export default {
->>>>>>> 91d564c9a5383a423d90ecb40f2c326649e3b322
 
 export default {
 
@@ -64,23 +47,41 @@ export default {
     VueEditor,
     ClientGrid
   },
-  data() {
+  data () {
     return {
       email: {
-        to: [],
+        from: null,
+        to: {
+          BccAddresses: [],
+          CcAddresses: [],
+          ToAddresses: []
+        },
         subject: null,
-        content: "<p>Hello,</p><p><br></p><p>This is an amazing email from OZMoSE!</p><p><br></p><p>Enjoy,</p><p>OSMoSe Team</p>"
-      }
+        content: '<p>Hello,</p><p><br></p><p>This is an amazing email from OSMoSE!</p><p><br></p><p>Enjoy,</p><p>OSMoSe Team</p>'
+      },
+      emailList: []
     };
   },
+  beforeMount () {
+    this.getIt();
+  },
   methods: {
-    saveContent() {
-      // You have the content to save
-      console.log(this.email);
-      this.email.to.push(this.email.sendTo);
-      this.email.content = "<html><head></head><body>" + this.email.content + "</body></html>";
-      console.log(this.email);
-      // let email = {to: [this.email.sendTo], subject: this.email.subject, lname:"L"};
+    saveContent () {
+      // this.email.to.push(this.email.sendTo);
+
+      let self = this;
+
+      self.email.content = '<html><head></head><body>' + self.email.content + '</body></html>';
+      self.emailList.forEach((sub) => {
+        if (sub.to) {
+          self.email.to.ToAddresses.push(sub.email);
+        } else if (sub.cc) {
+          self.email.to.CcAddresses.push(sub.email);
+        } else if (sub.bcc) {
+          self.email.to.BccAddresses.push(sub.email);
+        }
+      });
+
       this.$http.post('https://fqyy1uh5ui.execute-api.us-east-1.amazonaws.com/dev0/email', this.email)
         .then((res) => {
           console.log(res);
@@ -88,29 +89,21 @@ export default {
         }).catch((err) => {
           console.log(err);
         });
-<<<<<<< HEAD
+    },
+    getIt () {
+      let self = this;
+      this.$http.get('https://fqyy1uh5ui.execute-api.us-east-1.amazonaws.com/dev0/list', {})
+        .then((res) => {
+          console.log(res);
+          self.emailList = [];
+          res.data.emailList.forEach((e) => {
+            self.emailList.push({email: e.Email.S, fname: e.FirstName.S, lname: e.LastName.S, binary: e.EmailBinary.B, index: self.emailList.length});
+          });
+          console.log(self.emailList);
+        }).catch((err) => {
+          console.log(err);
+        });
     }
   }
 };
-=======
-    },
-      saveContent() {
-        // You have the content to save
-        console.log(this.email);
-        this.email.to.push(this.email.sendTo);
-        this.email.content = '<html><head></head><body>' + this.email.content + '</body></html>';
-        console.log(this.email);
-          // let email = {to: [this.email.sendTo], subject: this.email.subject, lname:"L"};
-        this.$http.post('https://fqyy1uh5ui.execute-api.us-east-1.amazonaws.com/dev0/email', this.email)
-            .then((res) => {
-              console.log(res);
-                // transition.next();
-            }).catch((err) => {
-              console.log(err);
-            });
-      }
-    }  
-};
-
->>>>>>> 91d564c9a5383a423d90ecb40f2c326649e3b322
 </script>
